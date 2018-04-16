ef9efad8fcccdbeefcc0c1afeec3eaf3d760806a
Maksim <maksalmak@gmail.com>
2018-04-16 13:17:49 +0300

    Added base EquipmentType and Equipment models

---
+++ Models/Equipments/Equipment.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Models.Equipments
+{
+    public class Equipment
+    {
+        public Guid Id { get; set; }
+        public string SerialNumber { get; set; }
+    }
+}
---
+++ Models/Equipments/EquipmentType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Models.Equipments
+{
+    public class EquipmentType
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
