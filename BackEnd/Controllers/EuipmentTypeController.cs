f0fe455291d63aa96cd79da6eae4db97df2886fe
Maksim <maksalmak@gmail.com>
2018-04-17 18:32:32 +0300

    First try write public API models on F#

---
+++ BackEnd/Controllers/EuipmentTypeController.cs
@@ -0,0 +1,49 @@
+﻿using BackEnd.DataBase;
+using Microsoft.AspNetCore.Mvc;
+using static Models.PublicAPI.Response;
+
+namespace BackEnd.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/EuipmentType")]
+    public class EuipmentTypeController : Controller
+    {
+        private readonly DataBaseContext dbContext;
+
+        public EuipmentTypeController(DataBaseContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET: api/EuipmentType
+        public Base Get()
+        {
+            return new Base(Models.PublicAPI.Response.StatusCode.OK);
+        }
+
+        // GET: api/EuipmentType/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/EuipmentType
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/EuipmentType/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models.PublicAPI/Responses/ResponseBase.fs
@@ -0,0 +1,7 @@
+﻿namespace Models.PublicAPI
+
+module Response =
+    type StatusCode = Unknown = 0 | OK = 1
+
+    type Base = {Status: StatusCode}
+
