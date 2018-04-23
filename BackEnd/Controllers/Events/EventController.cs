d922ba1a7968d5e1dc75b058c3e06febdc434128
Maksim <maksalmak@gmail.com>
2018-04-23 13:23:18 +0300

    Move controllers in sepatate folders

--- BackEnd/Controllers/Events/EventController.cs
+++ BackEnd/Controllers/Events/EventController.cs
@@ -17,7 +17,7 @@ using Models.PublicAPI.Responses.General;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace BackEnd.Controllers
+namespace BackEnd.Controllers.Events
 {
     [Produces("application/json")]
     [Route("api/Event")]
