0f477adc0a0a9796764ab9e5fb448e1cb0ec0632
Maksim <maksalmak@gmail.com>
2018-08-16 13:53:20 +0300

    Add production migration

---
+++ BackEnd/DataBase/Migrations/Production/20180816104915_WishAndInvites.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BackEnd.DataBase.Migrations.Production
+{
+    public partial class WishAndInvites : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserStatus",
+                table: "PlaceUserRole",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserStatus",
+                table: "PlaceUserRole");
+        }
+    }
+}
