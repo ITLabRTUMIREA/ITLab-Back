f3afefed20c0e11843ecfe2cf65ebe4fe54cdaaa
Maksim <maksalmak@gmail.com>
2018-08-28 07:38:55 +0300

    Production migration

--- BackEnd/DataBase/Migrations/Production/20180828043553_CreateToCreationTime.Designer.cs
+++ BackEnd/DataBase/Migrations/Production/20180828043553_CreateToCreationTime.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace BackEnd.DataBase.Migrations.Production
 {
     [DbContext(typeof(DataBaseContext))]
-    partial class DataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180828043553_CreateToCreationTime")]
+    partial class CreateToCreationTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BackEnd/DataBase/Migrations/Production/20180828043553_CreateToCreationTime.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BackEnd.DataBase.Migrations.Production
+{
+    public partial class CreateToCreationTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateTime",
+                table: "PlaceUserRole",
+                newName: "CreationTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreationTime",
+                table: "PlaceUserRole",
+                newName: "CreateTime");
+        }
+    }
+}
