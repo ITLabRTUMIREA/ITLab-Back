1d79166334bcea4618fc335b454c9f29f9a0e55f
Maksim <maksalmak@gmail.com>
2018-09-19 21:26:12 +0300

    Add EventRole model

---
+++ Models/DataBaseLinks/PlaceUserEventRole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models.Events;
+using Models.Events.Roles;
+using Models.People;
+
+namespace Models.DataBaseLinks
+{
+    public class PlaceUserEventRole
+    {
+        public Guid PlaceId { get; set; }
+        public Place Place { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public Guid EventRoleId { get; set; }
+        public EventRole EventRole { get; set; }
+
+        public UserStatus UserStatus { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime? DoneTime { get; set; }
+    }
+}
---
+++ Models/Events/Roles/EventRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models.DataBaseLinks;
+
+namespace Models.Events.Roles
+{
+    public class EventRole
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<PlaceUserEventRole> PlaceUserEventRoles { get; set; }
+    }
+}
