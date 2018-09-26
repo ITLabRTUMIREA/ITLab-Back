d24f6fa57d5df94dadda1928d6cb807da3c5eaf6
Maksim <maksalmak@gmail.com>
2018-09-26 22:53:50 +0300

    Start develop UserProperties

---
+++ Models/People/UserProperties/UserProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Models.People.UserProperties
+{
+    public class UserProperty
+    {
+        public Guid Id { get; set; }
+        public string Value { get; set; }
+        public bool IsConfirmed { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public Guid UserPropertyTypeId { get; set; }
+        public UserPropertyType UserPropertyType { get; set; }
+    }
+}
---
+++ Models/People/UserProperties/UserPropertyNames.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Models.People.UserProperties
+{
+    public enum UserPropertyNames
+    {
+        StudyGroup,
+        VkontakteId,
+        TelegramLink,
+        SkypeId
+    }
+}
