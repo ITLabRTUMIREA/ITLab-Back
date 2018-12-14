36ea62cf86850c936cbeafe4ac8aa952b63c7122
Artem <AsharnRus@mail.ru>
2018-12-14 17:25:05 +0300

    Add middle name

--- Models.PublicAPI/Requests/Account/AccountEditRequest.cs
+++ Models.PublicAPI/Requests/Account/AccountEditRequest.cs
@@ -5,6 +5,7 @@ namespace Models.PublicAPI.Requests.Account
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string MiddleName { get; set; }
         public string PhoneNumber { get; set; }
     }
 }
--- Models/People/User.cs
+++ Models/People/User.cs
@@ -11,6 +11,7 @@ namespace Models.People
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string MiddleName { get; set; }
         public List<UserSetting> UserSettings { get; set; }
         public List<Equipment> Equipment  { get; set; }
         public List<RefreshToken> RefreshTokens { get; set; }
