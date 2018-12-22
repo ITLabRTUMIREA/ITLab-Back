c006e9b1bc3e1c3a43704b9fac97c91f6b1d5dc3
Maksim <maksalmak@gmail.com>
2018-12-22 11:31:20 +0300

    Use enum type for define database

---
+++ BackEnd/Models/Settings/DbType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models.Settings
+{
+    public enum DbType
+    {
+        SQL_SERVER_REMOTE,
+        SQL_SERVER_LOCAL,
+        IN_MEMORY,
+        POSTGRES_LOCAL
+    }
+}
