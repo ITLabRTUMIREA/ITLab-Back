a6bc6c0617ee43364322bfc662d5fe63ee365bd3
Maksim Makushchenko <maksalmak@gmail.com>
2019-07-31 20:46:35 +0300

    Add notife service

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,10 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using IdentityServer4;
-using IdentityServer4.Models;
-using Newtonsoft.Json;
+﻿using IdentityServer4.Models;
 using System.Collections.Generic;
 
 namespace IdentityServer
@@ -26,35 +20,6 @@ namespace IdentityServer
             {
                 new ApiResource("api1", "My API #1")
             };
-        }
-
-        public static IEnumerable<Client> GetClients()
-        {
-            var a = 
-            new[]
-            {
-                new Client
-                {
-                    ClientId = "spa",
-                    ClientName = "IT Lab Site",
-                    AllowedGrantTypes = GrantTypes.Code,
-                    RequirePkce = true,
-                    RequireClientSecret = false,
-
-                    RedirectUris =           { "http://localhost:5500/callback.html" },
-                    PostLogoutRedirectUris = { "http://localhost:5500/index.html" },
-                    AllowedCorsOrigins =     { "http://localhost:5500" },
-
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "api1"
-                    }
-                }
-            };
-            System.Console.WriteLine(JsonConvert.SerializeObject(a));
-            return a;
         }
     }
 }
