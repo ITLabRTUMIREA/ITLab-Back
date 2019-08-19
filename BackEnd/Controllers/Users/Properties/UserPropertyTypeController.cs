c8baaaa534a7f104bae55730dd1951940c81686b
Maksim <maksalmak@gmail.com>
2019-08-19 16:40:13 +0300

    Add user property type editing action

--- BackEnd/Controllers/Users/Properties/UserPropertyTypeController.cs
+++ BackEnd/Controllers/Users/Properties/UserPropertyTypeController.cs
@@ -56,9 +56,9 @@ namespace BackEnd.Controllers.Users.Properties
         /// <returns>User property type</returns>
         /// <response code="200">Success</response>
         /// <response code="404">Not found item</response>
-        [HttpGet("{id:guid}")]
         [ProducesResponseType(200)]
         [ProducesResponseType(404)]
+        [HttpGet("{id:guid}")]
         public async Task<ActionResult<UserPropertyTypeView>> GetAsync(Guid id)
         {
             var targetType = await dbContext.UserPropertyTypes
@@ -78,11 +78,11 @@ namespace BackEnd.Controllers.Users.Properties
         /// <response code="201">Created new user property tye</response>
         /// <response code="400">Can't create user property type</response>
         /// <response code="409">Name of wanted type exists</response>
-        [HttpPost]
-        [RequireRole(RoleNames.CanEditUserPropertyTypes)]
         [ProducesResponseType(201)]
         [ProducesResponseType(400)]
         [ProducesResponseType(409)]
+        [HttpPost]
+        [RequireRole(RoleNames.CanEditUserPropertyTypes)]
         public async Task<ActionResult<UserPropertyTypeView>> PostAsync(
             [FromBody]UserPropertyTypeCreateRequest request)
         {
@@ -97,6 +97,43 @@ namespace BackEnd.Controllers.Users.Properties
         }
 
         /// <summary>
+        /// Edit user property type
+        /// </summary>
+        /// <param name="id">Id of property type to delete</param>
+        /// <returns>Id of deleted type</returns>
+        /// <response code="200">Success editing</response>
+        /// <response code="400">Can't edit user property type</response>
+        /// <response code="404">Not fount user property type</response>
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(409)]
+        [HttpPut("{id:guid}")]
+        [RequireRole(RoleNames.CanEditUserPropertyTypes)]
+        public async Task<ActionResult<Guid>> PutAsync(Guid id, [FromBody] UserPropertyTypeEditRequest request)
+        {
+            var targetUserPropertyType = await dbContext.UserPropertyTypes
+                .FindAsync(id);
+            if (targetUserPropertyType == null)
+                return NotFound();
+
+            mapper.Map(request, targetUserPropertyType);
+
+            try
+            {
+                var modiciedCount = await dbContext.SaveChangesAsync();
+                if (modiciedCount == 0)
+                    return BadRequest("Can't delete user property type");
+            }
+            catch (DbUpdateException dbException)
+            {
+                logger.LogWarning(dbException, "Error while deleting user property type");
+                return BadRequest("Can't delete user property type");
+            }
+            return id;
+        }
+
+        /// <summary>
         /// Delete user property type
         /// </summary>
         /// <param name="id">Id of property type to delete</param>
@@ -105,12 +142,12 @@ namespace BackEnd.Controllers.Users.Properties
         /// <response code="400">Can't delete user property type</response>
         /// <response code="404">Not fount user property type</response>
         /// <response code="409">Can't delete locked type</response>
-        [HttpDelete("{id:guid}")]
-        [RequireRole(RoleNames.CanEditUserPropertyTypes)]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
         [ProducesResponseType(409)]
+        [HttpDelete("{id:guid}")]
+        [RequireRole(RoleNames.CanEditUserPropertyTypes)]
         public async Task<ActionResult<Guid>> DeleteAsync(Guid id)
         {
             var targetUserPropertyType = await dbContext.UserPropertyTypes
--- Models.PublicAPI/Requests/User/Properties/UserPropertyType/UserPropertyTypeEditRequest.cs
+++ Models.PublicAPI/Requests/User/Properties/UserPropertyType/UserPropertyTypeEditRequest.cs
@@ -3,7 +3,7 @@ namespace Models.PublicAPI.Requests.User.Properties.UserPropertyType
 {
     public class UserPropertyTypeEditRequest : IdRequest
     {
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
     }
 }
